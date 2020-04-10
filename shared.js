129573d9d1444fde6a142ccd7649cf9d56a7c27b
jripke74 <jripke74@gmail.com>
2020-04-10 14:47:33 -0700

    add shared.js

---
+++ shared.js
@@ -0,0 +1,19 @@
+let backdrop = document.querySelector('.backdrop');
+let modal = document.querySelector('.modal');
+let modalNoButton = document.querySelector('.modal__action--negative');
+let selectPlanButtons = document.querySelectorAll('.plan button');
+
+for (let i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener('click', () => {
+    modal.style.display = 'block';
+    backdrop.style.display = 'block';
+  });
+}
+
+backdrop.addEventListener('click', closeModal);
+modalNoButton.addEventListener('click', closeModal);
+
+function closeModal() {
+  backdrop.style.display = 'none';
+  modal.style.display = 'none';
+}
